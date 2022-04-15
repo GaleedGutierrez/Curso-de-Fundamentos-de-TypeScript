9b2eefd951f4aa5936b648cc2432a3cac1318b3d
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-04-14 23:15:33 -0300

    Clase N°11: String.

---
+++ src/06-string.ts
@@ -0,0 +1,17 @@
+(() => {
+    let productTitle = 'My amazing product';
+    // productTitle = 1;
+    const productDescription = "It's my amazing product change";
+    console.log({productTitle});
+    const productPrice = 100;
+    const isNew = true;
+    const sumary = `
+        title: ${productTitle}
+        description: ${productDescription}
+        price: ${productPrice}
+        state: ${isNew}
+    `;
+    console.log({sumary});
+
+    const myString: string = '';
+})()
